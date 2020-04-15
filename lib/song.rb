3b93f220580b2a08c38020a8b21a9111b14832d9
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-15 19:23:20 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+attr_accessor :name, :artist, :genre 
+
+@@count = 0
+@@artists = []
+@@genres = []
+
+def initialize(name, artist, genre)
+@@count +=1
+@name = name
+@@artists << @artist = artist 
+@@genres << @genre = genre
+end
+
+def self.count 
+  @@count
+end
+
+def self.genres 
+  @@genres.uniq!
+end
+
+end
